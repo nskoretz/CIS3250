df3fada5f13dfc9b025e9503e74f8738a2fff733
Michael Vamvakas <mvamvaka@uoguelph.ca>
2017-10-23 16:55:11 -0400

    Added doxygen style commenting, changed all pointers so * values were next to variable names, changed struct types to pascal case

--- A2/boardGenerator.h
+++ A2/boardGenerator.h
@@ -22,55 +22,67 @@
  * Detailed explanation.
  */
 
-/* The pre-set dice which keep their values between games.*/
+/** 
+ * The pre-set dice which keep their values between games.
+ */
 typedef struct PresetDice {
-	char *configurations;
-	int position;
+    char *configurations;
+    int position;
 } PresetDice;
 
-/* The rolled dice which do not keep their values between games.*/
+/**
+ * The rolled dice which do not keep their values between games.
+ */
 typedef struct RolledDice {
-	char character;
-	int position;
+    char character;
+    int position;
 } RolledDice;
 
-/*
-Initializes the dice in the list of pre-set dice. Call once at program start.
-*/
-void initializePresetDice(struct presetDice* inputArrayOfDice);
+/**
+ * Description: Initializes the dice in the list of pre-set dice. Call once at program start.
+ * @param: inputArrayOfDice, pointer to a an array of the presetDice
+ */
+void initializePresetDice(struct PresetDice *inputArrayOfDice);
 
-/*
-Description: Rolls the dice to be used at the start of each game. 
-Puts the rolled dice in game_dice. Does not shuffle the dice. Helper function.
-*/
-void rollButNotShuffleDice(struct rolledDice* gameDice,
-		struct presetDice* inputArrayOfDice);
+/**
+ * Description: Rolls the dice to be used at the start of each game. 
+ * Puts the rolled dice in game_dice. Does not shuffle the dice. Helper function.
+ * @param: gameDice, pointer to the game dice
+ */
+void rollButNotShuffleDice(struct RolledDice *gameDice,
+        struct PresetDice* inputArrayOfDice);
 
-/*
-Shuffles an array of rolled dice. Helper function.
-*/
-void shuffleRolledDicePositions(struct rolledDice* gameDice);
+/**
+ * Shuffles an array of rolled dice. Helper function.
+ * @param: gameDice, pointer to the game dice
+ */
+void shuffleRolledDicePositions(struct RolledDice *gameDice);
 
-/*
-Prints a visualization of the input array of rolled_dice.
-*/
-void printGameBoard(struct rolledDice** gameBoard);
+/**
+ * Prints a visualization of the input array of rolled_dice.
+ * @param: gameBoard, pointer to an array of rolledDice
+ */
+void printGameBoard(struct RolledDice **gameBoard);
 
-/*
-Rolls the dice to be used at the start of the game and shuffles them, putting 
-the dice into the 2D array game_dice. Call once at the start of every game.
-*/
-void rollDice(struct rolledDice** gameBoard,
-		struct presetDice* inputArrayOfDice);
+/**
+ * Rolls the dice to be used at the start of the game and shuffles them, putting 
+ * the dice into the 2D array game_dice. Call once at the start of every game.
+ * @param: gameBoard, pointer to an array of rolledDice
+ */
+void rollDice(struct RolledDice **gameBoard,
+        struct PresetDice* inputArrayOfDice);
 
-/*
-Converts board from a string version to 2D array. Used in test mode.
-*/
+/**
+ * Converts board from a string version to 2D array. Used in test mode.
+ * @param: letters, string of all the letters
+ * @param: board, array of all the strings on the board
+ */
 void convertToBoard(char *letters, char ***board);
 
-/*
-prints out the contents from the boggle 2D array
-*/
+/**
+ * prints out the contents from the boggle 2D array
+ * @param: boggle: 2D array of characters
+ */
 void printHcBoard(char boggle[][4]);
 
 #endif /*BOARD_GEN_H*/
